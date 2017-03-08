241d18c7d8b26d848d6ab2bbde462a0d1cacea1e
xiefei <xiefei@gtan.com>
2017-03-08 12:19:42 +0800

    single collapsible with height animation

--- src/collapsible.vue
+++ src/collapsible.vue
@@ -1,32 +1,41 @@
 <template>
    <div class="parent">
       <div class="title" @click="toggleContent">{{title}}</div>
-      <transition name="scale">
-         <div class="content" v-show="contentVisible">{{content}}</div>
+      <transition name="slide"
+                  @enter="setHeight"
+                  @leave="clearHeight">
+         <div class="content" v-show="contentVisible" ref="content">{{content}}</div>
       </transition>
    </div>
 </template>
 
 <script>
   export default {
-    name: 'Collapsible',
+    name   : 'Collapsible',
     props  : {
       title  : String,
       content: String
     },
     data() {
       return {
-        contentVisible: true
+        contentVisible: true,
+        contentHeight : 'auto'
       }
     },
     methods: {
       toggleContent(){
-        if (this.contentVisible) {
-          this.contentVisible = false
-        } else {
-          this.contentVisible = true
-        }
+        this.contentVisible = !this.contentVisible
+      },
+      setHeight(el, done){
+        el.style.height = this.contentHeight
+      },
+      clearHeight(el, done){
+        el.style.height = 0
       }
+    },
+    mounted(){
+      this.contentHeight = window.getComputedStyle(this.$refs.content).height
+      this.$refs.content.style.height = this.contentHeight
     }
   }
 </script>
@@ -35,19 +44,14 @@
    .parent {
       border: 1px solid red;
       padding: 5px;
-      .title{
+      .title {
          border: 1px solid green;
          margin-bottom: 5px;
       }
-      .content{
-         border: 1px solid blue;
+      .content {
+         overflow-y: hidden;
+         transition: height .2s;
       }
    }
 
-   .scale-enter, .scale-leave-to {
-      transform: scaleY(0)
-   }
-   .scale-enter-active, .scale-leave-active{
-      transition: transform .2s;
-   }
 </style>
