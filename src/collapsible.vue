88b7a944f0db70a386e95d4e4c0ef5b3eb73870f
xiefei <xiefei@gtan.com>
2017-03-08 11:20:39 +0800

    single collapsible with scale animation

---
+++ src/collapsible.vue
@@ -0,0 +1,53 @@
+<template>
+   <div class="parent">
+      <div class="title" @click="toggleContent">{{title}}</div>
+      <transition name="scale">
+         <div class="content" v-show="contentVisible">{{content}}</div>
+      </transition>
+   </div>
+</template>
+
+<script>
+  export default {
+    name: 'Collapsible',
+    props  : {
+      title  : String,
+      content: String
+    },
+    data() {
+      return {
+        contentVisible: true
+      }
+    },
+    methods: {
+      toggleContent(){
+        if (this.contentVisible) {
+          this.contentVisible = false
+        } else {
+          this.contentVisible = true
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+   .parent {
+      border: 1px solid red;
+      padding: 5px;
+      .title{
+         border: 1px solid green;
+         margin-bottom: 5px;
+      }
+      .content{
+         border: 1px solid blue;
+      }
+   }
+
+   .scale-enter, .scale-leave-to {
+      transform: scaleY(0)
+   }
+   .scale-enter-active, .scale-leave-active{
+      transition: transform .2s;
+   }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Collapsible from './collapsible.vue'
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Collapsible, {props: {title: 'title', content: 'content'}})
 })
